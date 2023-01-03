f2d908b4c49f3296266cb2addf553a627d06b111
cjones1047 <cjmj1047@gmail.com>
2023-01-03 06:55:55 -0600

    exercise_endpoint correctly sends response from Nutritionix API based on user input"

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import os
 import dotenv
+import requests
 
 dotenv.load_dotenv()
 
 nutritionix_app_id = os.getenv("APP_ID")
 nutritionix_api_key = os.getenv("API_KEY")
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+headers = {
+    "x-app-id": nutritionix_app_id,
+    "x-app-key": nutritionix_api_key,
+    "Content-Type": "application/json"
+}
+
+json = {
+    "query": input("Tell me which exercise you did:\n")
+}
+
+exercise_response = requests.post(url=exercise_endpoint, headers=headers, json=json)
+print(exercise_response.text)
